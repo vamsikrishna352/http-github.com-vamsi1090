e0803169675ecf051ddc38c9112b5d44778d7f81
vamsikrishna352 <57290569+vamsikrishna352@users.noreply.github.com>
2019-11-03 23:11:21 -0800

    Update lcd (1).ino

--- lcd (1).ino
+++ lcd (1).ino
@@ -6,7 +6,7 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
   lcd.begin(16, 2);
-  lcd.print("Sushant kumar 19bec1078");
+  lcd.print("k.vamsi krishna 19BEC1090");
 }
 
 void loop() {
